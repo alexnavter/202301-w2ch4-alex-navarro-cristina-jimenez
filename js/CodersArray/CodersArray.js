bc7b5b0114bed498aa7e68fc2de15411f155a489
Cristina <cristinajmartin98@gmail.com>
2023-01-19 20:29:23 +0100

    add constructor to CodersArray

--- js/CodersArray/CodersArray.js
+++ js/CodersArray/CodersArray.js
@@ -1,7 +1,12 @@
 class CodersArray {
   length;
 
-  constructor(...elements) {}
+  constructor(...elements) {
+    for (let i = 0; i < elements.length; i++) {
+      this.i = elements;
+      this.length = elements.length;
+    }
+  }
 }
 
 export default CodersArray;
